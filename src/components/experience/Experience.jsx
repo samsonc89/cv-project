6563a0b1699f41ebffeed1b3a0738690da924a55
Samson Cheung <samsonc89@yahoo.com>
2023-04-10 10:30:30 -0700

    Create base elements for experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,7 +7,27 @@ class Experience extends Component {
   }
 
   render() {
-    return <div>Experience</div>;
+    return (
+      <div className="experience__container">
+        <div className="experience__heading component__input__heading">
+          <h2>Experience</h2>
+          <button>Submit/Edit</button>
+        </div>
+        <div className="experience__input__container">
+          <input type="text" placeholder="Company"></input>
+          <input type="text" placeholder="Position"></input>
+          <input type="text" placeholder="From MM/YY"></input>
+          <input type="text" placeholder="To MM/YY"></input>
+          <div className="experience__input--responsbility__wrapper">
+            <input type="text" placeholder="Responsibility"></input>
+            <button>+</button>
+            <button>-</button>
+          </div>
+        </div>
+        <button>Delete</button>
+        <button>Add</button>
+      </div>
+    );
   }
 }
 
