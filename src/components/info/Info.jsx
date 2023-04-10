5851292060d4e88f0e97913e98d1d829ba63e20f
Samson Cheung <samsonc89@yahoo.com>
2023-04-10 08:49:03 -0700

    Create base elements for personal information section

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -7,7 +7,22 @@ class Info extends Component {
   }
 
   render() {
-    return <div>Info</div>;
+    return (
+      <div className="info__container">
+        <div className="info__heading">
+          <h2>Personal Info</h2>
+          <button>Submit/Edit</button>
+        </div>
+        <div className="info__input__container">
+          <input type="text" placeholder="First Name"></input>
+          <input type="text" placeholder="Last Name"></input>
+          <input type="text" placeholder="Title"></input>
+          <input type="text" placeholder="LinkedIn"></input>
+          <input type="text" placeholder="Address"></input>
+          <input type="text" placeholder="Description"></input>
+        </div>
+      </div>
+    );
   }
 }
 
