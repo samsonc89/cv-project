77e838405d9246b8201f11b3eca02466da1d3675
Samson Cheung <samsonc89@yahoo.com>
2023-04-20 11:13:32 -0700

    Restructure code into a main component within App component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React, { Component } from "react";
-import {
-  Education,
-  Experience,
-  Footer,
-  Header,
-  Info,
-  Preview,
-} from "./components/index";
+import { Main, Footer, Header } from "./components/index";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { firstName: "", lastName: "", title: "" };
-
-    this.firstNameChange = this.firstNameChange.bind(this);
-    this.lastNameChange = this.lastNameChange.bind(this);
-    this.titleChange = this.titleChange.bind(this);
-  }
-
-  firstNameChange(e) {
-    this.setState({ firstName: e.target.value });
-  }
-  lastNameChange(e) {
-    this.setState({ lastName: e.target.value });
-  }
-  titleChange(e) {
-    this.setState({ title: e.target.value });
   }
 
   render() {
     return (
       <div className="app__container">
         <Header />
-        <div className="content__container section__padding">
-          <div className="input__container">
-            <Info
-              onFirstNameChanged={this.firstNameChange}
-              onLastNameChanged={this.lastNameChange}
-              onTitleChanged={this.titleChange}
-            />
-            <Experience />
-            <Education />
-          </div>
-          <div className="preview__container">
-            <Preview personalInfo={this.state} />
-          </div>
-        </div>
+        <Main />
         <Footer />
       </div>
     );
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
 import "./main.css";
+import { Info, Experience, Education, Preview } from "../index";
 
 class Main extends Component {
   constructor() {
     super();
+    this.state = { firstName: "", lastName: "", title: "" };
+
+    this.firstNameChange = this.firstNameChange.bind(this);
+    this.lastNameChange = this.lastNameChange.bind(this);
+    this.titleChange = this.titleChange.bind(this);
   }
 
+  firstNameChange(e) {
+    this.setState({ firstName: e.target.value });
+  }
+  lastNameChange(e) {
+    this.setState({ lastName: e.target.value });
+  }
+  titleChange(e) {
+    this.setState({ title: e.target.value });
+  }
   render() {
-    return <div className="main__container"></div>;
+    return (
+      <div className="main__container">
+        <div className="content__container section__padding">
+          <div className="input__container">
+            <Info
+              onFirstNameChanged={this.firstNameChange}
+              onLastNameChanged={this.lastNameChange}
+              onTitleChanged={this.titleChange}
+            />
+            <Experience />
+            <Education />
+          </div>
+          <div className="preview__container">
+            <Preview personalInfo={this.state} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
